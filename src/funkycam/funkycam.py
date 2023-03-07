84c55cc6463dcce7237abd5531fcfefbe8af8eac
opencv <thomaskhan444@gmail.com>
2023-03-06 21:09:56 -0500

    Added optimizations for processing images

--- src/funkycam/funkycam.py
+++ src/funkycam/funkycam.py
@@ -3,20 +3,21 @@ import random
 import cv2
 import numpy as np
 import seaborn as sns
+from PIL import Image
 
 class Funk(object):
-    def __init__( self, n_colors:int = 7, sns_color_palette:str = "rainbow", 
-                    random_colors:bool = False, color_list:list = [], 
+    def __init__( self, n_colors:int = 7, sns_color_palette:str = "rainbow",
+                    random_colors:bool = False, color_list:list = [],
                     line_size:int = 17, edge_blur_val:int = 9, color_blur_val:int = 27 ):
         self.n_colors = n_colors
         assert n_colors > 1, f"Number of colors must be greater than 1. Input was {n_colors}"
-        
+
         self.color_palette = sns_color_palette
         self.random_colors = random_colors
         self.color_list = color_list
         if len(color_list) > 0:
             self.n_colors = len(color_list)
-        
+
         self.line_size = line_size
         self.edge_blur_val = edge_blur_val
         self.color_blur_val = color_blur_val
@@ -24,8 +25,8 @@ class Funk(object):
         self.luminance_mult = [0.114, 0.587, 0.299]
         # self.luminance_mult = [0.0722, 0.7152, 0.2126] # alternative luminance formula
 
-        self.colors, self.lightness = self._color_lightness()     
-    
+        self.colors, self.lightness = self._color_lightness()
+
     def _color_lightness(self):
         if(self.random_colors):
             colors = np.random.randint(0, 255, size=(self.n_colors,3))
@@ -37,11 +38,11 @@ class Funk(object):
             color_pal = np.uint8(np.multiply(np.array(palette), 255))
             colors = color_pal.copy()
             colors = color_pal[:,::-1] # get bgr
-    
+
         lightness = np.uint8(np.sum(np.multiply(colors, self.luminance_mult), axis=1))
         colors = colors[np.argsort(lightness)]
         lightness = np.sort(lightness)
-        
+
         print("Colors:\n", colors)
 
         return colors, lightness
@@ -52,7 +53,7 @@ class Funk(object):
         gray_blur = cv2.GaussianBlur(gray, (self.edge_blur_val, self.edge_blur_val), -1)
         edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, self.line_size, self.edge_blur_val)
         return edges
-    
+
     def pick_color(self, img, colorsums, num_colors):
         # reassigning pixels based on brightness
         # adjusting the colors based on how brightness is visually seen by humans
@@ -66,15 +67,36 @@ class Funk(object):
                 condlist.append(sumX<colorsums[i])
             else:
                 condlist.append(sumX>colorsums[i])
-        
+
         inds = np.select(condlist, choicelist)
-        
+
         return inds
-    
+
     def funkify(self, img):
-        
-        edges = self.edge_mask(img)
-        blur = cv2.GaussianBlur(img,(self.color_blur_val, self.color_blur_val), sigmaX=0, sigmaY=0)
+        print('funkify!')
+        # Read in foreground image from the 'examples/resources' folder
+        background_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        background_img = Image.fromarray(background_img)
+        foreground_img_path = '../../example/resources/pic.jpeg'
+        foreground_img = Image.open(foreground_img_path)
+
+        # Convert image to RGBA because we need to control the alpha parameter when making the image
+        # transparent
+        foreground_img = foreground_img.convert("RGBA")
+        background_img = background_img.convert("RGBA")
+
+        # Blend foreground image with the background image
+        # The random number between 0 and 1 controls how transparent the foreground image is on the
+        # background
+        background_img = background_img.resize(foreground_img.size)
+        img_blend = Image.blend(background_img, foreground_img, random.uniform(0, 1))
+
+        # Save image before funkifying (to check it out)
+        img_blend.save("new.png","PNG")
+        img_blend = cv2.imread('new.png')
+
+        edges = self.edge_mask(img_blend)
+        blur = cv2.GaussianBlur(img_blend,(self.color_blur_val, self.color_blur_val), sigmaX=0, sigmaY=0)
         indices = self.pick_color(blur.reshape((-1, 3)), self.lightness, self.n_colors)
         recolored = np.uint8(self.colors[indices].reshape(blur.shape))
 
---
+++ src/funkycam/test.py
@@ -0,0 +1,55 @@
+from funkycam import Funk
+import cv2
+
+"""
+Testing image
+"""
+funk = Funk()
+
+background_img_path = '../../example/resources/willem.jpg'
+img = cv2.imread(background_img_path)
+result = funk.funkify(img)
+cv2.imshow('test', result)
+cv2.waitKey(0)
+
+"""
+Testing video
+"""
+# Use the video
+# specify Seaborn color_palette https://matplotlib.org/stable/tutorials/colors/colormaps.html
+funk = Funk(n_colors=8, sns_color_palette='inferno', line_size=11, edge_blur_val=5, color_blur_val=7)
+
+# manually choose colors
+# colors = [[128, 0, 64],[255,255,255],[164,73,163],[39,127,255],[36,28,237],[201,174,255],[234,217,153]]
+# funk = Funk(color_list=colors, line_size=11, edge_blur_val=5, color_blur_val=7)
+
+# generate random colors
+# funk = Funk(n_colors=7, random_colors=True, line_size=11, edge_blur_val=5, color_blur_val=7)
+
+vid_capture = cv2.VideoCapture('../../example/resources/KillBill.mp4')
+
+frame_width = int(vid_capture.get(3))
+frame_height = int(vid_capture.get(4))
+frame_size = (frame_width,frame_height)
+fps = vid_capture.get(cv2.CAP_PROP_FPS)
+
+output = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (frame_width, frame_height))
+
+while(vid_capture.isOpened()):
+    ret, frame = vid_capture.read()
+
+    if ret == True:
+        # Write the frame to the output files
+        funky_img = funk.funkify(frame)
+        output.write(funky_img)
+        cv2.imshow('video', funky_img)
+    else:
+        print('Stream disconnected')
+        break
+
+	# Press Q on keyboard to exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+vid_capture.release()
+output.release()
